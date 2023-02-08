d020d280d2832201326f1d9bb840e4c58c21bf8a
Abhishek Raj Gupta <abhishekrajgupta00@gmail.com>
2023-02-08 21:32:31 +0530

    route by link added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2 m-2 card">
-      <a className="navbar-brand s-16px " href="/">
+      <Link className="navbar-brand s-16px" to="/">
         Home
-      </a>
-      <a className="navbar-brand s-16px" href="/favourite">
+      </Link>
+      <Link className="navbar-brand s-16px" to="/favourite">
         Favourite
-      </a>
+      </Link>
     </nav>
   );
 }
