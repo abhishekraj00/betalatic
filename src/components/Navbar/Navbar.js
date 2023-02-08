64386e2435cdbde1cfaec8ff17b3f18314a2f748
Abhishek Raj Gupta <abhishekrajgupta00@gmail.com>
2023-02-08 22:48:02 +0530

    Sized fixed of nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,13 +8,13 @@ function Navbar() {
   };
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2 m-2 card">
-      <Link className="navbar-brand s-16px" to="/">
+      <Link className="navbar-brand " to="/">
         Home
       </Link>
-      <Link className="navbar-brand s-16px" to="/favourite">
+      <Link className="navbar-brand " to="/favourite">
         Favourite
       </Link>
-      <a href="/" className="navbar-brand s-16px" onClick={handleClear}>
+      <a href="/" className="navbar-brand " onClick={handleClear}>
         Clear localStorage
       </a>
     </nav>
