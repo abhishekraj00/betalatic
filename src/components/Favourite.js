35100e365e9a0f09d0926b2f212940c12f21144f
Abhishek Raj Gupta <abhishekrajgupta00@gmail.com>
2023-02-08 23:02:17 +0530

    clear local

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -8,21 +8,28 @@ function Favourite() {
   //univarsal store data
   const pakeges = useContext(PakagesStore);
   const { store } = pakeges;
+  const handleClear = () => {
+    localStorage.clear();
+  };
   return (
     <>
-      <>
-        <Navbar />
-        <div className="card p-2 m-2 text-center">
+      <Navbar />
+      <div className="card p-2 m-2 text-center">
+        <div className="card btn-collection">
           <h5
             className="p-2 m-2 card"
             style={{ backgroundColor: "black", color: "white" }}
           >
             Favourite List Of NPM Pakeges
           </h5>
-          {/* function is passing the favourite list array only */}
-          <Display displayData={getFavList(store)} />
+          <button href="/" className="btn btn-light " onClick={handleClear}>
+            Clear localStorage
+          </button>
         </div>
-      </>
+
+        {/* function is passing the favourite list array only */}
+        <Display displayData={getFavList(store)} />
+      </div>
     </>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,20 +3,14 @@ import { Link } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
-  const handleClear = () => {
-    localStorage.clear();
-  };
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2 m-2 card">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-black p-2 m-2 card">
       <Link className="navbar-brand " to="/">
         Home
       </Link>
       <Link className="navbar-brand " to="/favourite">
         Favourite
       </Link>
-      <a href="/" className="navbar-brand " onClick={handleClear}>
-        Clear localStorage
-      </a>
     </nav>
   );
 }
